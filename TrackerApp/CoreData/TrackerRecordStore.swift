963ccc04e570c9358577b6cbf20fecfd31a42c2d
Maxim Papier <maxim-paper@yandex.ru>
2023-04-13 17:24:51 +0600

    - Implemented TrackerRecordStore to manage CRUD operations of CoreData on TrackerRecord model - Deleted createdAt attribute form TrackerRecordData as redundant

--- TrackerApp/CoreData/TrackerRecordStore.swift
+++ TrackerApp/CoreData/TrackerRecordStore.swift
@@ -9,6 +9,67 @@ final class TrackerRecordStore {
         self.context = context
     }
 
-    // Add CRUD here
+    // MARK: - CRUD methods
+
+    func createTrackerRecord(record: TrackerRecord) {
+
+        let coreDataRecord = coreDataTrackerRecord(from: record)
+        do {
+            try context.save()
+        } catch {
+            print("Error saving tracker record: \(error)")
+        }
+    }
+
+    func readTrackerRecord() -> [TrackerRecord] {
+
+        let request: NSFetchRequest<TrackerRecordData> = TrackerRecordData.fetchRequest()
+        do {
+            let coreDataRecords = try context.fetch(request)
+            return coreDataRecords.compactMap( { trackerRecords(from: $0)  } )
+        } catch {
+            print("Error fetching tracker records: \(error)")
+            return []
+        }
+    }
+
+    func deleteTrackerRecord(by id: UUID) {
+
+        let request: NSFetchRequest<TrackerRecordData> = TrackerRecordData.fetchRequest()
+        request.predicate = NSPredicate(format: "id == $0", id as CVarArg)
+
+        do {
+            let coreDataRecords = try context.fetch(request)
+            guard let coreDataRecord = coreDataRecords.first else { return }
+            context.delete(coreDataRecord)
+            try context.save()
+        } catch {
+            print("Error deleting tracker record: \(error)")
+        }
+    }
+
+
+    // MARK: - Conversion methods
+
+    func coreDataTrackerRecord(from record: TrackerRecord) -> TrackerRecordData {
+
+        let coreDataRecord = TrackerRecordData(context: context)
+        coreDataRecord.id = record.id
+        coreDataRecord.date = record.date
+
+        return coreDataRecord
+    }
+
+    func trackerRecords(from coreDataRecord: TrackerRecordData) -> TrackerRecord? {
+
+        guard
+            let id = coreDataRecord.id,
+            let date = coreDataRecord.date
+        else {
+            return nil
+        }
+
+        return TrackerRecord(id: id, date: date)
+    }
 
 }
